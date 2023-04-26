7ca038983379822dd0bc4e7a0cd488d1e6200bee
khaled975 <khaledbebo975@gmail.com>
2023-04-27 01:45:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let slider=document.querySelector(".slider")
+let rightArrow=document.querySelector("#right")
+let leftArrow=document.querySelector("#left")
+let box = document.querySelector(".box").offsetWidth
+
+let drag=false
+
+const drgStart=()=>{
+    drag=true
+    slider.classList.add("dragging")
+}
+const dragging=(e)=>{
+    if(!drag) return ;
+    slider.scrollLeft=e.pageX
+    console.log(e.pageX);
+}
+const drgEnd=()=>{
+    drag=false
+    slider.classList.remove("dragging")
+}
+function move(e){
+    if(e==='right'){
+        slider.scrollLeft+=box + 33
+    }else{
+        slider.scrollLeft-=box + 33
+
+    }
+}
+
+slider.addEventListener("mousemove",dragging)
+slider.addEventListener("mousedown",drgStart)
+slider.addEventListener("mouseup",drgEnd)
